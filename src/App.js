f566d67c019597d386b81c4812efbb0ceb245b63
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-10-18 22:29:23 -0400

    Adding dynamically changing background

--- src/App.js
+++ src/App.js
@@ -38,7 +38,12 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className = {
+      (typeof weather.main != "undefined") ? 
+      ((weather.main.temp > 16) ? 
+      'App warm' : 'App') 
+      : 'App'}>
+
       <main>
         <div className="search-box">
           <input 
